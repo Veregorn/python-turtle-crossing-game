a701a0352914958ce4bec2a226ca5436b1e9815c
Veregorn <rjbarco@gmail.com>
2024-01-16 14:56:57 +0100

    Now we have a turtle placed in the initial position

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+# Variables Setup
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+p1 = Player()
+
+
 
 game_is_on = True
 while game_is_on:
